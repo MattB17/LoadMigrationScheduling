83bde621493fae787b5a3830ba7d9e13920a2133
MattB17 <matt.buckley1755@gmail.com>
2021-10-29 18:34:55 -0400

    Adding matrix model example, documenting parser

---
+++ Examples/matrix1.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3.6
+"""This example formulates and solves the following simple MIP model using the
+matrix API:
+        max x + y + 2z
+    s.t.
+        x + 2y + 3z <= 4
+        x +  y      >= 1
+        x, y, z binary
+
+"""
+import gurobipy as gp
+from gurobipy import GRB
+import numpy as np
+import scipy.sparse as sp
+
+if __name__ == "__main__":
+    try:
+        # create a new model
+        m = gp.Model("matrix1")
+
+        # create variables
+        x = m.addMVar(shape=3, vtype=GRB.BINARY, name="x")
+
+        # set objective
+        obj = np.array([1.0, 1.0, 2.0])
+        m.setObjective(obj @ x, GRB.MAXIMIZE)
+
+        # build (sparse) constraint matrix
+        val = np.array([1.0, 2.0, 3.0, -1.0, -1.0])
+        row = np.array([0, 0, 0, 1, 1])
+        col = np.array([0, 1, 2, 0, 1])
+
+        A = sp.csr_matrix((val, (row, col)), shape=(2, 3))
+
+        # build RHS vector
+        rhs = np.array([4.0, -1.0])
+
+        # Add constraints
+        m.addConstr(A @ x <= rhs, name="c")
+
+        # Optimize model
+        m.optimize()
+
+        # print variables and objective value
+        print(x.X)
+        print("Objective value: {0}".format(m.objVal))
+    except gp.GurobiError as e:
+        print("Error code {0}: {1}".format(e.errno, e))
+    except AttributeError:
+        print("Encountered an attribute error")
--- MigrationScheduling/Parser.py
+++ MigrationScheduling/Parser.py
@@ -1,10 +1,72 @@
+"""The `Parser` class is used to parse a file containing data specifying a
+migration instance.
+
+"""
+
 class Parser:
+    """Used to parse files containing a migration instance.
+
+    Attributes
+    ----------
+    _migrations: dict
+        A dictionary of migrations. The keys are strings specifying the
+        name of the switch involved in the migration and the keys are
+        `Migration` objects representing the migrations.
+    _controller_constraints: set
+        A set of `ControllerConstraint` objects.
+    _qos_constraints: set
+        A set of `QosConstraint` objects.
+
+    """
     def __init__(self):
         self._migrations = {}
         self._controller_constraints = set()
         self._qos_constraints = set()
 
-    def add_migration(self, migration_data):
+    def parse_migrations(self, migration_file):
+        """Parses `migration_file` for the data of a migration instance.
+
+        The object is updated with the data for the migration instance.
+
+        Parameters
+        ----------
+        migration_file: str
+            The name of the file containing the data of a migration instance.
+
+        Returns
+        -------
+        None
+
+        """
+        with open(migration_file, 'r') as data_file:
+            for line in data_file:
+                if (line[0] == "s"):
+                    self._add_migration(line.strip().split(" "))
+                elif (line[0] == "c"):
+                    self._add_controller_constraint(line.strip().split(" "))
+                elif (line[0] == "g"):
+                    self._add_qos_constraint(line.strip().split(" "))
+        return
+
+    def _add_migration(self, migration_data):
+        """Adds a migration to the parser based on `migration_data`.
+
+        A `Migration` object is created and added to `_migrations`. The key
+        is a string representing the name of the switch in the migration.
+
+        Parameters
+        ----------
+        migration_data: list
+            A list of strings specifying the data for the migrations. The
+            first three elements identify the switch, destination controller,
+            and the load of the migration. All subsequent elements identify
+            the QoS groups to which the migration belongs.
+
+        Returns
+        -------
+        None
+
+        """
         migration = Migration(
             migration_data[0], migration_data[1], float(migration_data[2]))
         curr_idx = 3
@@ -14,7 +76,25 @@ class Parser:
             curr_idx += 1
         self._migrations[migration_data[0]] = migration
 
-    def add_controller_constraint(self, controller_data):
+    def _add_controller_constraint(self, controller_data):
+        """Adds a new controller constraint based on `controller_data`.
+
+        A new `ControllerConstraint` object is constructed and added to
+        `_controller_constraints`.
+
+        Parameter
+        ---------
+        controller_data: list
+            A two element list containing the data for a controller
+            constraint. The first is a string representing the name of the
+            controller and the second is a string representing the
+            controller's capacity.
+
+        Returns
+        -------
+        None
+
+        """
         constraint = ControllerConstraint(
             controller_data[0], float(controller_data[1]))
         for migration in self._migrations:
@@ -22,20 +102,26 @@ class Parser:
                 constraint.add_switch(migration.get_switch())
         self._controller_constraints.add(constraint)
 
-    def add_qos_constraint(self, qos_data):
-        constraint = QosConstraint(qos_data[0], qos_data[1])
+    def _add_qos_constraint(self, qos_data):
+        """Adds a QoS constraint based on `qos_data`.
+
+        A new `QoSConstraint` is added to `_qos_constraints`.
+
+        Parameters
+        ----------
+        qos_data: list
+            A two element list specifying the data for a QoS constraint. The
+            first element is a string representing the name of the QoS group
+            and the second is a string representing the number of concurrent
+            migrations allowed within the QoS group.
+
+        Returns
+        -------
+        None
+        
+        """
+        constraint = QosConstraint(qos_data[0], int(qos_data[1]))
         for migration in self._migrations:
             if migration.is_in_group(qos_data[0]):
                 constraint.add_switch(migration.get_switch())
         self._qos_constraints.add(constraint)
-
-    def parse_migrations(self, migration_file):
-        with open(migration_file, 'r') as data_file:
-            for line in data_file:
-                if (line[0] == "s"):
-                    self.add_migration(line.strip().split(" "))
-                elif (line[0] == "c"):
-                    self.add_controller_constraint(line.strip().split(" "))
-                elif (line[0] == "g"):
-                    self.add_qos_constraint(line.strip().split(" "))
-        return
