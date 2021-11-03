092bdd11271f3ecddeb68dc753ec44a24220205a
MattB17 <matt.buckley1755@gmail.com>
2021-11-03 16:35:44 -0400

    Implementing the IP model in the optimizer

--- MigrationScheduling/Model/Optimizer.py
+++ MigrationScheduling/Model/Optimizer.py
@@ -3,7 +3,9 @@ migration schedule.
 
 """
 from MigrationScheduling.Model import Parser
+from MigrationScheduling import exceptions as exc
 from MigrationScheduling import utils
+import gurobipy as gp
 
 class Optimizer:
     """Builds an optimization model for a migration scheduling instance.
@@ -56,8 +58,132 @@ class Optimizer:
         """
         self._parser = Parser()
         self._parser.parse_migrations(migration_file)
-        self._switch_ids = parser.get_switch_ids()
+        self._switch_ids = self._parser.get_switch_ids()
         self._round_ids = list(range(utils.upper_bound_rounds(
             len(self._switch_ids))))
-        self._controller_ids = parser.get_controller_ids()
-        self._group_ids = parser.get_group_ids()
+        self._controller_ids = self._parser.get_controller_ids()
+        self._group_ids = self._parser.get_group_ids()
+
+    def _initialize_variables(self, is_ip=True):
+        """Initializes the variables for the optimization model.
+
+        Parameters
+        ----------
+        is_ip: bool
+            A boolean flag indicating whether it is an IP model. If true
+            the variables will be integer, otherwise, they will be continuous.
+
+        Returns
+        -------
+        gp.Var, gp.Vars
+            A variable representing the parameter to be optimized and a
+            collection of variables representing which round each migration
+            is scheduled in.
+
+        """
+        if self._model:
+            lambda_var = self._model.addVar(name="lambda")
+            if is_ip:
+                x_vars = self._model.addVars(
+                    self._switch_ids, self._round_ids,
+                    vtype=gp.GRB.BINARY, name="x")
+            else:
+                x_vars = self._model.addVars(
+                    self._switch_ids, self._round_ids, lb=0.0, ub=1.0,
+                    vtype=gp.GRB.CONTINUOUS, name="x")
+            return lambda_var, x_vars
+        raise exc.UninitializedModel()
+
+    def _add_migrate_constraints(self, x_vars):
+        """Adds the migrate constraints using `x_vars`.
+
+        The migrate constraints enforces that each migration takes place
+        in exactly one round.
+
+        Parameters
+        ----------
+        x_vars: gp.Vars
+            The set of model variables used in the migrate constraints.
+
+        Returns
+        -------
+        None
+
+        """
+        self._model.addConstrs((x_vars.sum(i, '*') == 1
+                                for i in self._switch_ids), "migrate")
+
+    def _add_bound_constraints(self, lambda_var, x_vars):
+        """Adds the constraints bounding `lambda_var`.
+
+        A constraint is introduced for each round and migration, enforcing
+        the constraint that if migration i is scheduled in round r then
+        `lambda_var` must be at least r.
+
+        Parameters
+        ----------
+        lambda_var: gp.Var
+            The variable that is being bounded.
+        x_vars: gp.Vars
+            The collection of variables used to bound `lambda_var`.
+
+        Returns
+        -------
+        None
+
+        """
+        self._model.addConstrs(
+            (r * x_vars[i, r] <= lambda_var
+             for i in self._switch_ids for r in self._round_ids), "bound")
+
+    def _add_controller_constraints(self, x_vars):
+        """Adds the set of controller constraints to the model using `x_vars`.
+
+        Parameters
+        ----------
+        x_vars: gp.Vars
+            The set of model variables used in the controller constraints.
+
+        Returns
+        -------
+        None
+
+        """
+        for controller_const in self._parser.get_controller_constraints():
+            for r in self._round_ids:
+                self._model.addConstr(sum(
+                    self._parser.get_migrations()[s].get_load() *
+                    x_vars[self._parser.get_migrations()[s].get_switch_idx(), r]
+                    for s in controller_const.get_switches()) <= controller_const.get_capacity(),
+                    "controller[{0}, {1}]".format(controller_const.get_controller_idx(), r))
+
+    def _add_qos_constraints(self, x_vars):
+        """Adds the set of QoS constraints to the model using `x_vars`.
+
+        Parameters
+        ----------
+        x_vars: gp.Vars
+            The set of model variables used in the controller constraints.
+
+        Returns
+        -------
+        None
+
+        """
+        for qos_const in self._parser.get_qos_constraints():
+            for r in self._round_ids:
+                self._model.addConstr(sum(
+                    x_vars[self._parser.get_migrations()[s].get_switch_idx(), r]
+                    for s in qos_const.get_switches()) <= qos_const.get_capacity(),
+                    "qos[{0}, {1}]".format(qos_const.get_group_idx(), r))
+
+    def build_model(self, migration_file):
+        self.get_model_data(migration_file)
+        self._model = gp.Model("migration")
+        lambda_var, x_vars = self._initialize_variables()
+        self._model.setObjective(lambda_var, gp.GRB.MINIMIZE)
+        self._add_migrate_constraints(x_vars)
+        self._add_bound_constraints(lambda_var, x_vars)
+        self._add_controller_constraints(x_vars)
+        self._add_qos_constraints(x_vars)
+        self._model.optimize()
--- MigrationScheduling/exceptions.py
+++ MigrationScheduling/exceptions.py
@@ -6,7 +6,7 @@ load migration schedule.
 class InvalidName(Exception):
     """Exception raised when an invalid object name is generated.
 
-    Attributes
+    Parameters
     ----------
     message: str
         A string representing the message displayed to the user.
@@ -14,3 +14,14 @@ class InvalidName(Exception):
     """
     def __init__(self, message):
         super().__init__(message)
+
+class UninitializedModel(Exception):
+    """Exception raised when an model has not been initialized.
+
+    The Exception is raised when a user tries to access a model that has
+    not yet been initialized.
+
+    """
+    def __init__(self):
+        super().__init__("Must initialize model before adding variables, " +
+                         "an objective function, and constraints")
