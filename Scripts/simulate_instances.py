8da7a803173fd2c6f5fbf2e2525fbb34eae6be5c
MattB17 <matt.buckley1755@gmail.com>
2021-11-08 12:03:00 -0500

    Modifying optimizer with more efficient code for QoS constraints and modifying script

--- Scripts/simulate_instances.py
+++ Scripts/simulate_instances.py
@@ -5,15 +5,21 @@ import os
 import sys
 from MigrationScheduling.Data import Simulator
 
-NUM_MIGRATIONS_LIST = [5, 10, 20, 50, 100, 500, 1000, 5000, 10000]
+SMALL_INSTANCES = [5, 10, 20, 50, 100, 250, 500, 1000, 5000, 10000]
 FILE_IDX = 1
 
 if __name__ == "__main__":
     output_path = sys.argv[1]
     simulator = Simulator()
-    curr_idx = FILE_IDX
+    curr_idx_small = FILE_IDX
+    curr_idx_large = 0
     for num_migrations in NUM_MIGRATIONS_LIST:
-        simulator.run(
-            num_migrations,
-            os.path.join(output_path, "migrations{}.txt".format(curr_idx)))
-        curr_idx += 1
+        if num_migrations <= 250:
+            output_file = os.path.join(
+                output_path, "migrations{}.txt".format(curr_idx_small))
+            curr_idx_small += 1
+        else:
+            output_file = os.path.join(
+                output_path, "migrations_large{}.txt".format(curr_idx_large))
+            curr_idx_large += 1
+        simulator.run(num_migrations, output_file)
