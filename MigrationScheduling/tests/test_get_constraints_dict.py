30731646ce2da37cafc991df4c53cda44ce7ff6e
MattB17 <matt.buckley1755@gmail.com>
2021-11-22 20:53:33 -0500

    Testing get_controller_constraint_dicts and get_constraints_dict

---
+++ MigrationScheduling/tests/test_get_constraints_dict.py
@@ -0,0 +1,46 @@
+from unittest.mock import patch, MagicMock
+from MigrationScheduling.utils import get_constraints_dict
+
+
+CONTROL_STR = "MigrationScheduling.utils.get_controller_constraint_dicts"
+QOS_STR = "MigrationScheduling.utils.get_qos_constraint_dicts"
+
+
+@patch(QOS_STR)
+@patch(CONTROL_STR)
+def test_simple_instance(mock_control_dicts, mock_qos_dicts):
+    mock_data = MagicMock()
+    mock_data.get_qos_consts = MagicMock(return_value=set())
+    control_const = MagicMock()
+    mock_control_dicts.return_value = {'c0': control_const}
+    mock_qos_dicts.return_value = {}
+    assert get_constraints_dict(mock_data) == {'c0': control_const}
+    mock_control_dicts.assert_called_once_with(mock_data)
+    mock_data.get_qos_consts.assert_called_once()
+    mock_qos_dicts.assert_called_once_with(set())
+
+
+@patch(QOS_STR)
+@patch(CONTROL_STR)
+def test_complex_instance(mock_control_dicts, mock_qos_dicts):
+    mock_data = MagicMock()
+    control_consts = [MagicMock() for _ in range(3)]
+    qos_consts = [MagicMock() for _ in range(4)]
+    mock_data.get_qos_consts = MagicMock(return_value=set(qos_consts))
+    mock_control_dicts.return_value = {'c1': control_consts[0],
+                                       'c5': control_consts[1],
+                                       'c7': control_consts[2]}
+    mock_qos_dicts.return_value = {'g0': qos_consts[0],
+                                   'g1': qos_consts[1],
+                                   'g2': qos_consts[2],
+                                   'g3': qos_consts[3]}
+    assert get_constraints_dict(mock_data) == {'c1': control_consts[0],
+                                               'c5': control_consts[1],
+                                               'c7': control_consts[2],
+                                               'g0': qos_consts[0],
+                                               'g1': qos_consts[1],
+                                               'g2': qos_consts[2],
+                                               'g3': qos_consts[3]}
+    mock_control_dicts.assert_called_once_with(mock_data)
+    mock_data.get_qos_consts.assert_called_once()
+    mock_qos_dicts.assert_called_once_with(set(qos_consts))
---
+++ MigrationScheduling/tests/test_get_controller_constraint_dicts.py
@@ -0,0 +1,66 @@
+import pytest
+from unittest.mock import call, patch, MagicMock
+from MigrationScheduling.utils import get_controller_constraint_dicts
+
+
+CONTROL_STR = "MigrationScheduling.utils.get_constraint_dict_for_controller"
+
+
+@pytest.fixture(scope="function")
+def mock_control_const():
+    control_const = MagicMock()
+    control_const.get_controller = MagicMock(return_value='c0')
+    return control_const
+
+
+@patch(CONTROL_STR)
+def test_with_no_constraints(mock_const_dict):
+    mock_data = MagicMock()
+    mock_data.get_migrations = MagicMock(return_value={'s3': MagicMock()})
+    mock_data.get_control_consts = MagicMock(return_value=set())
+    assert get_controller_constraint_dicts(mock_data) == {}
+    mock_data.get_migrations.assert_called_once()
+    mock_data.get_control_consts.assert_called_once()
+    mock_const_dict.assert_not_called()
+
+
+@patch(CONTROL_STR)
+def test_with_one_constraint(mock_const_dict, mock_control_const):
+    mock_data = MagicMock()
+    migrations = [MagicMock() for _ in range(3)]
+    mock_data.get_migrations = MagicMock(return_value={
+        's0': migrations[0], 's2': migrations[1], 's5': migrations[2]})
+    mock_data.get_control_consts = MagicMock(
+        return_value={mock_control_const})
+    const_dict = MagicMock()
+    mock_const_dict.return_value = const_dict
+    assert get_controller_constraint_dicts(mock_data) == {'c0': const_dict}
+    mock_data.get_migrations.assert_called_once()
+    mock_data.get_control_consts.assert_called_once()
+    mock_const_dict.assert_called_once_with(
+        mock_control_const, set(migrations))
+    mock_control_const.get_controller.assert_called_once()
+
+
+@patch(CONTROL_STR)
+def test_with_multi_constraints(mock_const_dict, mock_control_const):
+    mock_data = MagicMock()
+    migrations = [MagicMock() for _ in range(3)]
+    mock_data.get_migrations = MagicMock(return_value={
+        's1': migrations[0], 's9': migrations[1], 's11': migrations[2]})
+    control_consts = [mock_control_const, MagicMock(), MagicMock()]
+    control_consts[1].get_controller = MagicMock(return_value='c3')
+    control_consts[2].get_controller = MagicMock(return_value='c7')
+    mock_data.get_control_consts = MagicMock(return_value=control_consts)
+    const_dicts = (MagicMock(), MagicMock(), MagicMock())
+    mock_const_dict.side_effect = const_dicts
+    assert get_controller_constraint_dicts(mock_data) == {
+        'c0': const_dicts[0], 'c3': const_dicts[1], 'c7': const_dicts[2]}
+    mock_data.get_migrations.assert_called_once()
+    mock_data.get_control_consts.assert_called_once()
+    for control_const in control_consts:
+        control_const.get_controller.assert_called_once()
+    dict_calls = [call(control_const, set(migrations))
+                  for control_const in control_consts]
+    assert mock_const_dict.call_count == 3
+    mock_const_dict.assert_has_calls(dict_calls)
