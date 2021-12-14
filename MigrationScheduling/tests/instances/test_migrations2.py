b974d3868cce583463fbb5e604afde38ef41db26
MattB17 <matt.buckley1755@gmail.com>
2021-12-14 17:22:27 -0500

    Adding another test instance with 10 migrations from the LogNormalSimulator and adding tests for it

--- MigrationScheduling/tests/instances/test_migrations2.py
+++ MigrationScheduling/tests/instances/test_migrations2.py
@@ -8,56 +8,41 @@ DIR = os.path.dirname(os.path.dirname(
         os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 
 # indices of network objects
-SWITCH_IDS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
-ROUND_IDS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
-CONTROLLER_IDS = [0, 1, 2, 3, 4]
-GROUP_IDS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+SWITCH_IDS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+ROUND_IDS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+CONTROLLER_IDS = [0, 1, 2]
+GROUP_IDS = [0, 1, 2, 3, 4, 5]
 
 # switch loads
-LOADS = {0: 13.24,
-         1: 8.83,
-         2: 17.9,
-         3: 7.65,
-         4: 7.68,
-         5: 11.21,
-         6: 1.38,
-         7: 4.94,
-         8: 5.46,
-         9: 17.33,
-         10: 10.34,
-         11: 7.28,
-         12: 4.25,
-         13: 7.00,
-         14: 6.99}
+LOADS = {0: 5.19,
+         1: 2.15,
+         2: 13.19,
+         3: 1.7,
+         4: 1.71,
+         5: 3.46,
+         6: 0.48,
+         7: 0.99,
+         8: 1.1,
+         9: 11.77}
 
 # controller capacities
-CONTROLLER_CAPS = {0: 50.19,
-                   1: 23.19,
-                   2: 51.41,
-                   3: 11.78,
-                   4: 38.35}
+CONTROLLER_CAPS = {0: 20.00, 1: 17.27, 2: 35.27}
 
 # group capacities
-GROUP_CAPS = {0: 1, 1: 1, 2: 2, 3: 5, 4: 2, 5: 1, 6: 1, 7: 1, 8: 1, 9: 2}
+GROUP_CAPS = {0: 2, 1: 3, 2: 1, 3: 1, 4: 1, 5: 2}
 
 # switches migrating to each destination controller
-DST_CONTROLLERS = {0: {2, 3},
-                   1: {13, 14},
-                   2: {4, 6, 8, 9, 11, 12},
-                   3: {5},
-                   4: {0, 1, 7, 10}}
+DST_CONTROLLERS = {0: {0, 1, 2, 4},
+                   1: {5, 6, 7, 8},
+                   2: {3, 9}}
 
 # group membership
-GROUPS = {0: {0, 7, 14},
-          1: {0, 1, 2, 3, 4, 7, 11, 14},
-          2: {0, 3, 12, 14},
-          3: {0, 1, 2, 7, 9, 11, 14},
-          4: {0, 2, 13, 14},
-          5: {0, 3, 12, 14},
-          6: {0, 2, 3, 14},
-          7: {7, 12, 14},
-          8: {0, 2, 14},
-          9: {0, 2, 3, 9, 11, 12, 14}}
+GROUPS = {0: {0, 2, 3, 7},
+          1: {0, 3, 7, 9},
+          2: {0},
+          3: {2},
+          4: {0, 2},
+          5: {0, 1, 3}}
 
 
 def test_optimizer():
@@ -66,16 +51,17 @@ def test_optimizer():
     x_vars = m.addVars(SWITCH_IDS, ROUND_IDS, vtype=GRB.BINARY, name="x")
     lambda_var = m.addVar(name="lambda")
     m.setObjective(lambda_var, GRB.MINIMIZE)
-    m.addConstrs((x_vars.sum(i, '*') == 1 for i in SWITCH_IDS), 'migrate')
+    m.addConstrs((x_vars.sum(i, '*') == 1 for i in SWITCH_IDS), "migrate")
     m.addConstrs((r * x_vars[i, r] <= lambda_var
                   for i in SWITCH_IDS for r in ROUND_IDS), "bounds")
     m.addConstrs((sum(LOADS[i] * x_vars[i, r] for i in DST_CONTROLLERS[j])
                   <= CONTROLLER_CAPS[j]
-                  for j in CONTROLLER_IDS for r in ROUND_IDS),
-                  "controller_cap")
+                  for j in CONTROLLER_IDS for r in ROUND_IDS
+                  if len(DST_CONTROLLERS[j]) > 0),
+                 "controller_cap")
     m.addConstrs((sum(x_vars[i, r] for i in GROUPS[l]) <= GROUP_CAPS[l]
                   for l in GROUP_IDS for r in ROUND_IDS),
-                  "group_cap")
+                 "group_cap")
     m.optimize()
 
     # optimizer
@@ -91,13 +77,7 @@ def test_vff_heuristic():
         os.path.join("instances", "migrations2.txt")))
     vff_val = algorithms.vector_first_fit(optimizer.instance_data())
 
-    # the VFF solution has value 8:
-    # - migrations 0, 5, 6, 8, 9, 10, and 13 are scheduled in round 1
-    # - migrations 1, and 12 are scheduled in round 2
-    # - migration 2 is scheduled in round 3
-    # - migration 3 is scheduled in round 4
-    # - migration 4 is scheduled in round 5
-    # - migration 7 is scheduled in round 6
-    # - migration 11 is scheduled in round 7
-    # - migration 14 is scheduled in round 8
-    assert vff_val == 8
+    # the VFF solution has value 2:
+    # - migrations 0, 1, 4, 5, 6, 7, 8, 9 are scheduled in round 1
+    # - migrations 2 and 3 are scheduled in round 2
+    assert vff_val == 2
